779c4279264c467626d3448e4911ff0845c74b59
hieunc <hieunc@aoi-sys.vn>
2015-01-29 13:10:04 +0700

    #6

--- site/protected/controllers/ContentController.php
+++ site/protected/controllers/ContentController.php
@@ -51,27 +51,14 @@ class ContentController extends Controller
 	/**
 	 * Displays the contact page
 	 */
-	public function actionContact()
+	public function actionAbout()
 	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
-				$headers="From: $name <{$model->email}>\r\n".
-					"Reply-To: {$model->email}\r\n".
-					"MIME-Version: 1.0\r\n".
-					"Content-Type: text/plain; charset=UTF-8";
+		$this->setTitle('Giới thiệu');
+		$this->breadcrumbs = array(
+			'Giới thiệu',
+		);
 
-				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
+		$this->render('index');
 	}
 
 	/**
