15d8503dd1a79d70034ff94de0e5a434fd2f294d
hieunc <hieunc18@gmail.com>
2015-01-29 23:52:44 +0700

    #11

--- site/protected/config/database.php
+++ site/protected/config/database.php
@@ -5,10 +5,10 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=anthanh',
+	'connectionString' => 'mysql:host=db05.serverhosting.vn;dbname=ant5b7af_anthanh',
 	'emulatePrepare' => true,
-	'username' => 'asvadmin',
-	'password' => 'abcd1234',
+	'username' => 'ant5b7af_anthanh',
+	'password' => '123456',
 	'charset' => 'utf8',
 
 );
